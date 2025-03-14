9a111c8f4f25733a49e131b7b9ce823bd07a111b
8088yyy <8088yyy@gmail.com>
2025-03-14 18:39:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,16 +8,6 @@ header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
 
-// Check referrer to ensure it's coming from the allowed domain
-$allowed_referrer = 'https://8088y.site/';
-$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-
-// Verify the referrer starts with the allowed domain
-if (strpos($referrer, $allowed_referrer) !== 0) {
-    header('HTTP/1.0 403 Forbidden');
-    die("Access denied: Invalid referrer");
-}
-
 // Fetch the DASH stream URL
 $get = isset($_GET['get']) ? $_GET['get'] : '';
 if (!$get) {
@@ -25,12 +15,13 @@ if (!$get) {
 }
 
 $mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/' . $get;
+
 $mpdheads = [
-    'http' => [
-        'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
-        'follow_location' => 1,
-        'timeout' => 5
-    ]
+  'http' => [
+      'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
 ];
 
 $context = stream_context_create($mpdheads);
